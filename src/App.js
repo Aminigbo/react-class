da347be3c4cae8619cdb1e9be7435e38648c2215
aminigbo <aminigbopaul@gmail.com>
2025-02-04 12:23:38 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import logo from './logo.svg';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import { Button, Form, Header, Input } from './global/component';
-import Home from './home/pages';
+import Home from './home/pages'; 
 import { AuthProvider } from './global/auth-context';
+
 function App() {
   return (
     <AuthProvider>
       <div className="App">
         <Header />
-        <Home />
+        <Routes>
+          <Route path="/" element={<Home />} /> 
+        </Routes>
       </div>
     </AuthProvider>
   );
